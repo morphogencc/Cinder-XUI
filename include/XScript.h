6391c79970ae37ce7d676b362658dd47ef305863
nathan lachenmyer <nathan@morphogen.cc>
2020-11-16 13:40:31 -0500

    tracking wmf-video

--- include/XScript.h
+++ include/XScript.h
@@ -3,9 +3,9 @@
 #include "cinder/app/App.h"
 
 extern "C" {
-	#include "lua/lauxlib.h"
-	#include "lua/lua.h"
-	#include "lua/lualib.h"
+	#include "lauxlib.h"
+	#include "lua.h"
+	#include "lualib.h"
 }
 #include "LuaBridge/LuaBridge.h"
 
